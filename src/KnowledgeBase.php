c0874d249afd5d8b231bdcbdb12820a530e529f2
sinkcup <sinkcup@live.it>
2017-05-06 11:37:48 +0800

    new: create kb. close bothub-ai/qna-maker-sdk-php#1

---
+++ src/KnowledgeBase.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Microsoft\QnAMaker;
+
+use GuzzleHttp\Client;
+
+class KnowledgeBase
+{
+    private $client;
+
+    private $conf = array(
+        'base_uri' => 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/',
+        'timeout'  => 3.0,
+        'subscription_key' => '',
+    );
+
+    public function __construct($conf)
+    {
+        if (empty($conf) || !isset($conf['subscription_key']) || empty($conf['subscription_key'])) {
+            throw new Exception('need: subscription_key');
+        }
+        $this->conf = array_merge($this->conf, $conf);
+        $this->client = new Client([
+            'base_uri' => $this->conf['base_uri'],
+            'timeout' => $this->conf['timeout'],
+            'headers' => [
+                'Ocp-Apim-Subscription-Key' => $this->conf['subscription_key'],
+            ],
+        ]);
+    }
+
+    /**
+     * Create Knowledge Base
+     *
+     * @example shell curl -X POST -d '{"name": "Learn English", "qnaPairs":[{"answer": "Fine, thanks.", "question": "how are you?"},{"answer": "Nice to meet you, too.", "question": "nice to meet you"}, "urls": ["http://www.seattle.gov/hala/faq"]]}' -H 'Content-Type: application/json' -H 'Ocp-Apim-Subscription-Key: {subscription key}' https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/create
+     * @return array
+     */
+    public function store($name, $qnaPairs = [], $urls = [])
+    {
+        if (empty($name)) {
+            throw new Exception('need: name(not empty)');
+        }
+        $data = [
+            'name' => $name,
+            'qnaPairs' => !empty($qnaPairs) ? $qnaPairs : [
+                ['answer' => 'Hello', 'question' => 'hi'],
+            ], // QnA bug: return 400 "BadArgument Request Body" when only send "name"，so send one question at here
+        ];
+        if (!empty($urls)) {
+            $data['urls'] = $urls;
+        }
+        try {
+            $response = $this->client->request('POST', 'create', [
+                'json' => $data,
+            ]);
+        } catch (\Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+        return json_decode($response->getBody(), true);
+    }
+}
---
+++ tests/KnowledgeBaseTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Microsoft\QnAMaker\Tests;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Microsoft\QnAMaker\KnowledgeBase;
+
+class KnowledgeBaseTest extends TestCase
+{
+    private $kb;
+
+    protected function setUp()
+    {
+        $this->kb = new KnowledgeBase([
+            'subscription_key' => getenv('QNA_SUB_KEY'),
+            'timeout' => 10,
+        ]);
+    }
+
+    public function testStore()
+    {
+        $name = 'Learn English';
+        $qnaPairs = [
+            ['answer' => 'Fine, thanks.', 'question' => 'how are you?'],
+            ['answer' => 'Nice to meet you, too.', 'question' => 'nice to meet you'],
+        ];
+        $urls = ['http://www.seattle.gov/hala/faq', 'https://example.com/'];
+        $r = $this->kb->store($name . ' - only name');
+        $this->assertArrayHasKey('kbId', $r);
+
+        $r = $this->kb->store($name . ' - name and qnaParis', $qnaPairs);
+        $this->assertArrayHasKey('kbId', $r);
+
+        $r = $this->kb->store($name . ' - name and urls', [], $urls);
+        $this->assertArrayHasKey('kbId', $r);
+        $dataExtractionResults = [
+            [
+                "sourceType" => "Url",
+                "extractionStatusCode" => "Success",
+                "source" => "http://www.seattle.gov/hala/faq"
+            ],
+            [
+                "sourceType" => "Url",
+                "extractionStatusCode" => "NoQuestionsFound",
+                "source" => "https://example.com/"
+            ],
+        ];
+        $this->assertEquals($dataExtractionResults, $r['dataExtractionResults']);
+
+        $r = $this->kb->store($name . ' - name, qnaPairs and urls', $qnaPairs, $urls);
+        $this->assertArrayHasKey('kbId', $r);
+        $this->assertEquals($dataExtractionResults, $r['dataExtractionResults']);
+    }
+}
